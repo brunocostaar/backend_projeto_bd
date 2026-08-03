2a56712baefa085be52fb6acff14d7fa9fd590ca
Bruno Costa <brunoo.lcn@gmail.com>
2026-08-02 22:28:35 -0300

    fixes

--- database.py
+++ database.py
@@ -4,9 +4,16 @@ from collections.abc import Iterator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
+try:
+    from dotenv import load_dotenv
+    env_path = os.path.join(os.path.dirname(__file__), ".env")
+    load_dotenv(dotenv_path=env_path)
+except ImportError:
+    pass
+
 SQLALCHEMY_DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://postgres:postgres@localhost:5432/hospital_universitario",
+    "postgresql://postgres:postgres@localhost:5439/hospital_universitario",
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
